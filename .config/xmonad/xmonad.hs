536705c9faaf7e6c72108154bc205c6c27ee6d6d
Glauber Santana <glauber.silva14@gmail.com>
2022-12-31 19:15:27 -0300

    add: layout hook and keybindings

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,16 +1,47 @@
 import XMonad
-import XMonad.Util.EZConfig
+import XMonad.Util.Ungrab
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.SpawnOnce
+
+import XMonad.Layout.Magnifier
+import XMonad.Layout.ThreeColumns
 
 -- Changing default terminal emulator
 myTerminal = "alacritty"
--- Setting Mod Key from Alt to Super
+
+-- Rebinding Mod Key from Alt to Super
+myModMask :: KeyMask
 myModMask = mod4Mask
--- Window borders width
-myBorderWidth = 0
+
+-- Window borders
+myBorderWidth :: Dimension
+myBorderWidth = 1
+
+myBorderColor, myFocusedBorderColor :: String
+myBorderColor  = "gray"
+myFocusedBorderColor = "purple"
+
+-- Layouts
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = magnifiercz' 1.3 (ThreeColMid nmaster delta ratio)
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
 
 main :: IO ()
-main = xmonad def {
-	terminal = myTerminal,
-	modMask = myModMask,
-	borderWidth = myBorderWidth
-}
+main = xmonad $ def {
+  terminal = myTerminal,
+  modMask = myModMask,
+  layoutHook = myLayout,
+  borderWidth = myBorderWidth,
+  normalBorderColor  = myBorderColor,
+  focusedBorderColor = myFocusedBorderColor
+}`additionalKeysP`
+    [ ("M-S-z", spawn "xscreensaver-command -lock")
+    , ("Print", unGrab *> spawn "scrot -s"        )
+    , ("M-f"  , spawn "qutebrowser"               )
+    ]
+
