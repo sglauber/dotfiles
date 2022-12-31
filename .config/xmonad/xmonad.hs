a40b944e2d6f2410e22486cbf8c8e731ea5f0137
Glauber Santana <glauber.silva14@gmail.com>
2022-12-31 20:03:46 -0300

    add: pretty-printer and logger handler

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -17,7 +17,7 @@ main :: IO ()
 main = xmonad
      . ewmhFullscreen
      . ewmh
-     . withEasySB (statusBarProp "xmobar ~/.config/xmobar/xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
+     . withEasySB (statusBarProp "xmobar ~/.config/xmobar/.xmobarrc" (pure myXmobarPP)) defToggleStrutsKey
      $ myConfig
   where
     toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
@@ -25,6 +25,32 @@ main = xmonad
 
 myXmobarPP :: PP
 myXmobarPP = def
+    { ppSep             = magenta " • "
+    , ppTitleSanitize   = xmobarStrip
+    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden          = white . wrap " " ""
+    , ppHiddenNoWindows = lowWhite . wrap " " ""
+    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    }
+  where
+    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+    -- | Windows should have *some* title, which should not not exceed a
+    -- sane length.
+    ppWindow :: String -> String
+    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+    blue, lowWhite, magenta, red, white, yellow :: String -> String
+    magenta  = xmobarColor "#ff79c6" ""
+    blue     = xmobarColor "#bd93f9" ""
+    white    = xmobarColor "#f8f8f2" ""
+    yellow   = xmobarColor "#f1fa8c" ""
+    red      = xmobarColor "#ff5555" ""
+    lowWhite = xmobarColor "#bbbbbb" ""
+
 
 -- Changing default terminal emulator
 myTerminal = "alacritty"
@@ -38,8 +64,8 @@ myBorderWidth :: Dimension
 myBorderWidth = 1
 
 myBorderColor, myFocusedBorderColor :: String
-myBorderColor  = "gray"
-myFocusedBorderColor = "purple"
+myBorderColor  = "purple"
+myFocusedBorderColor = "magenta"
 
 -- Layouts
 
