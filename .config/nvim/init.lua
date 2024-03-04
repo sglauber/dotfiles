3a8043b415d0cfb94e2275ee2392e4627909fbd1
Glauber Santana <glauber.silva14@gmail.com>
2024-03-04 10:24:58 -0300

    refactor: move plugins configs from init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,11 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    vim.fn.system { 'git','clone', '--filter=blob:none','--branch=stable',lazyrepo, lazypath }
 end
 vim.opt.rtp:prepend(lazypath)
 
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 require("lonen.set")
 require("lonen.remap")
 require("lonen.autocmds")
