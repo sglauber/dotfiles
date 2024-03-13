40d9670be8d462ed9e631ea848841a4048ab8213
Glauber S <glauber.silva14@gmail.com>
2024-03-13 20:53:34 -0300

    chore: remind neovim to remind me to remind to not use Arrows

--- .config/nvim/lua/lonen/remap.lua
+++ .config/nvim/lua/lonen/remap.lua
@@ -43,3 +43,7 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
