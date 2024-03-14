bb05447a2b698982d6e40bce2c32c56720357b0e
Glauber S <glauber.silva14@gmail.com>
2024-03-14 13:34:20 -0300

    feat: add alejandra

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,31 +4,45 @@
   # This defines sources that flakes needs to fetch
   inputs = {
     nixpkgs = {
-    	url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+    };
   };
 
   # This defines nix code that are evaluated after fetching the inputs
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = {
+    self,
+    alejandra,
+    nixpkgs,
+    ...
+  } @ inputs: {
     nixosConfigurations = {
-    	default = nixpkgs.lib.nixosSystem {
+      default = nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
-		specialArgs = {inherit inputs;};
-		# These are chunks of nix code that extends the configuration
-		modules = [
-			./hosts/default/configuration.nix
-			inputs.home-manager.nixosModules.default {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.lonen = import ./hosts/default/home.nix;
-                home-manager.extraSpecialArgs = { inherit inputs; };
-            }
-		];
-	};
-	};
+        specialArgs = {inherit inputs;};
+        # These are chunks of nix code that extends the configuration
+        modules = [
+          {
+            environment.systemPackages = [alejandra.defaultPackage.${system}];
+          }
+          ./hosts/default/configuration.nix
+          inputs.home-manager.nixosModules.default
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lonen = import ./hosts/default/home.nix;
+            home-manager.extraSpecialArgs = {inherit inputs;};
+          }
+        ];
+      };
+    };
   };
 }
