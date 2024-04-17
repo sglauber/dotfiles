339d6315b54c5b904b9d95e4f31394dd706e29d5
sglauber <glauber.silva14@gmail.com>
2024-04-17 19:38:10 -0300

    feat: Add flake-parts, formatter and new inputs

--- flake.nix
+++ flake.nix
@@ -1,109 +1,116 @@
 {
-    description = "Lonen's Floquinhos";
-	inputs = {
-		# Core
-		nixpkgs = {
-		    url = "github:nixos/nixpkgs/nixos-unstable";
-		};
+  description = "Lonen's Floquinhos";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/master";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-		hyprland = {
-			url = "github:hyprwm/hyprland";
-		};
+      imports = [ ./hosts ];
 
-		waybar-hyprland = { 
-			url = "github:hyprwm/hyprland";
-		};
-
-		xdg-portal-hyprland = {
-			url = "github:hyprwm/xdg-desktop-portal-hyprland";
-		};
-		
-		nur = {
-			url = "github:nix-community/NUR";
-		};
-		
-		nix-colors = {
-			url = "github:misterio77/nix-colors";
-		};
-
-		spicetify-nix = {
-			url = "github:the-argus/spicetify-nix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-
-		disko = {
-			url = "github:nix-community/disko";
-		};
-		
-		neovim-nightly-overlay = {
-			url = "github:nix-community/neovim-nightly-overlay";
-		};
-		
-		nix-ld-rs = {
-			url = "github:nix-community/nix-ld-rs";
-		};
-
-		# SFMono w/ patches
-		sf-mono-liga-src = {
-			url = "github:shaunsingh/SFMono-Nerd-Font-Ligaturized";
-			flake = false;
-		};
-  };
-
-  outputs = {
-    self,
-    nixpkgs,
-    hyprland,
-    home-manager,
-    spicetify-nix,
-    disko,
-    ...
-  } @ inputs: let
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-
-    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-    # Nixpkgs instantiated for supported system types.
-    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
-  in {
-  	nixosConfigurations = {
-		anchor = nixpkgs.lib.nixosSystem {
-		system = "x86_64-linux";
-		specialArgs = { inherit inputs hyprland spicetify-nix disko; };
-          	modules = [
-		    ./hosts/anchor/configuration.nix
-		    home-manager.nixosModules.home-manager 
-		    {
-		        home-manager = {
-			    useUserPackages = true;
-			    useGlobalPkgs = false;
-			    extraSpecialArgs = { inherit inputs spicetify-nix disko; };
-			    users.lonen = ./home/desktop/home.nix;
-			};
-            	    }
-		    hyprland.nixosModules.default
-		    { programs.hyprland.enable = true; }
-		    disko.nixosModules.disko
-          	];
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              packages = [
+                pkgs.alejandra
+                pkgs.git
+                config.packages.repl
+              ];
+              name = "nixland";
+              DIRENV_LOG_FORMAT = "";
+            };
+          };
+          formatter = pkgs.alejandra;
         };
     };
-    devShells = forAllSystems (system: let
-      pkgs = nixpkgsFor.${system};
-    in {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          git
-          alejandra
-          statix
-        ];
-      };
-    });
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    aesthetic-iosevka.url = "github:alphatechnolog/aesthetic-iosevka";
+
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fu.url = "github:numtide/flake-utils";
+
+    hm = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprfocus = {
+      url = "github:pyt0xic/hyprfocus";
+      inputs.hyprland.follows = "hyprland";
+    };
+
+    hypridle.url = "github:hyprwm/hypridle";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    hyprland-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprlock.url = "github:hyprwm/hyprlock";
+
+    hyprpaper.url = "github:hyprwm/hyprpaper";
+
+    matugen = {
+      url = "github:InioX/matugen";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "fu";
+    };
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "hm";
+    };
+
+    spicetify-nix = {
+      url = "github:MichaelPachec0/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sss = {
+      url = "github:SergioRibera/sss/";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    xdg-portal-hyprland.url = "github:hyprwm/xdg-desktop-portal-hyprland";
+
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+    };
+
+    sf-mono-liga-src = {
+      url = "github:shaunsingh/SFMono-Nerd-Font-Ligaturized";
+      flake = false;
+    };
   };
-}
