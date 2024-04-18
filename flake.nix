59efb897a3124c78be4ee3128837d68903370871
Glauber Santana <glauber.silva14@gmail.com>
2024-04-18 13:09:29 -0300

    feat: add home-manager to flakes

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
 
-      imports = [./hosts];
+      imports = [./hosts ./home];
 
       perSystem = {
         config,
