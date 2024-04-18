adc45d911f43b8aa54b3a00d0211b73df6bc6dac
Glauber Santana <glauber.silva14@gmail.com>
2024-04-18 13:16:52 -0300

    feat: add initialPassword and user description

--- system/core/users.nix
+++ system/core/users.nix
@@ -1,7 +1,9 @@
 {pkgs, ...}: {
   users.users.lonen = {
     isNormalUser = true;
+    description = "Glauber Santana";
     shell = pkgs.zsh;
+    initialPassword = "notasecret";
     extraGroups = [
       "audio"
       "networkmanager"
