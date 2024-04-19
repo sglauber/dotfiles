ab5abeed22089e795946628d676feafd1f0d2429
Glauber Santana <glauber.silva14@gmail.com>
2024-04-18 23:17:38 -0300

    feat: add cliphist, matugen, vesktop and gtk settings

--- home/software/default.nix
+++ home/software/default.nix
@@ -1,5 +1,16 @@
 {
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = [
     ./terminal
+    ./gtk.nix
+  ];
+
+  home.packages = with pkgs; [
+    cliphist
+    vesktop
+    inputs.matugen.packages.${pkgs.system}.default
   ];
 }
