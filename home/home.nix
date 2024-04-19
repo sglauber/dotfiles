62bfd46198703d56df25ffe596b8076a94a0074d
Glauber Santana <glauber.silva14@gmail.com>
2024-04-18 23:16:50 -0300

    feat: add colors, matugen, hyprlock and hypridle

--- home/home.nix
+++ home/home.nix
@@ -5,9 +5,13 @@
   ...
 }: {
   imports = [
-    # ./specialisations.nix
+    ./specialisations.nix
     ./software
+    inputs.matugen.nixosModules.default
     inputs.nix-index-db.hmModules.nix-index
+    inputs.hyprlock.homeManagerModules.default
+    inputs.hypridle.homeManagerModules.default
+    self.nixosModules.theme
   ];
   home = {
     username = "lonen";
@@ -24,4 +28,10 @@
 
   # let HM manage itself when in standalone mode
   programs.home-manager.enable = true;
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      lib = prev.lib // {colors = import "${self}/lib/colors" lib;};
+    })
+  ];
 }
