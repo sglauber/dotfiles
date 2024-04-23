14acc97c54a3f4e32a1fc5570d03a298b97afd88
Glauber Santana <glauber.silva14@gmail.com>
2024-04-22 21:08:03 -0300

    feat: locale settings

--- system/core/default.nix
+++ system/core/default.nix
@@ -3,17 +3,13 @@
 {
   imports = [
     ./boot.nix
+    ./locale.nix
     ./security.nix
     ./users.nix
   ];
 
-  i18n.defaultLocale = "en_US.UTF-8";
-
   # don't touch this
   system.stateVersion = lib.mkDefault "23.11";
-
-  time.timeZone = lib.mkDefault "America/Bahia";
-  time.hardwareClockInLocalTime = lib.mkDefault true;
 
   # compresses half the ram for use as swap
   zramSwap = {
