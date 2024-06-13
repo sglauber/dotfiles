d4cc7ffcd9f04c790d859f93ca91219ecada7344
Glauber Santana <glauber.silva14@gmail.com>
2024-06-13 14:46:16 -0300

    feat: add nixvim

--- home/editors/neovim/default.nix
+++ home/editors/neovim/default.nix
@@ -1,1 +1,14 @@
-_: {}
+{inputs, ...}: {
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+    ./plugins
+    ./options.nix
+  ];
+  home.shellAliases.v = "nvim";
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+}
