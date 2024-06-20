39e5ce9bb2386b95d34b6fc21df98f106b1fd43c
Glauber Santana <glauber.silva14@gmail.com>
2024-06-20 16:43:45 -0300

    feat: add declarative networks

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -1,13 +1,33 @@
-{lib, ...}: {
-  networking = {
-    networkmanager = {
-      enable = true;
-      wifi.powersave = false;
+{config, ...}: {
+  sops.secrets.wireless = {
+    sopsFile = ../../hosts/shared/secrets.yaml;
+    neededForUsers = true;
+  };
+
+  networking.wireless = {
+    enable = true;
+    fallbackToWPA2 = false;
+    environmentFile = config.sops.secrets.wireless.path;
+    networks = {
+      "UAIFAI" = {
+        pskRaw = "@UAIFAI@";
+      };
+
+      "UAIFAI_5G" = {
+        pskRaw = "@UAIFAI5G@";
+      };
     };
 
-    enableIPv6 = false;
-    dhcpcd.wait = "background";
-    dhcpcd.extraConfig = "noarp";
+    # Imperative
+    allowAuxiliaryImperativeNetworks = true;
+    userControlled = {
+      enable = true;
+      group = "network";
+    };
+
+    extraConfig = ''
+      update_config=1
+    '';
   };
 
   services = {
@@ -16,4 +36,9 @@
       settings.UseDns = true;
     };
   };
+
+  # Ensure group exists
+  users.groups.network = {};
+
+  systemd.services.wpa_supplicant.preStart = "touch /etc/wpa_supplicant.conf";
 }
