d7092020eb7f0415966f96a62182ce25b620dbb6
Glauber Santana <glauber.silva14@gmail.com>
2024-06-29 14:09:09 -0300

    feat: move telescope settings to its own namespace

---
+++ home/editors/neovim/plugins/telescope/default.nix
@@ -0,0 +1,5 @@
+_: {
+  imports = [
+    ./telescope.nix
+  ];
+}
---
+++ home/editors/neovim/plugins/telescope/telescope.nix
@@ -0,0 +1,55 @@
+_: {
+  programs.nixvim = {
+    plugins.telescope = {
+      enable = true;
+
+      extensions = {
+        fzf-native.enable = true;
+      };
+
+      keymaps = {
+        "<leader>ff" = {
+          action = "find_files";
+          options.desc = "[S]earch Project [F]iles";
+        };
+
+        "<leader>sh" = {
+          action = "help_tags";
+          options.desc = "[S]earch [H]elp Pages";
+        };
+
+        "<leader>sD" = {
+          action = "diagnostics";
+          options.desc = "[S]earch Workspace [D]iagnostics";
+        };
+
+        "<leader>sb" = {
+          action = "buffers";
+          options.desc = "[S]earch [B]uffers";
+        };
+
+        "<leader>sg" = {
+          action = "git_files";
+          options.desc = "[S]earch [G]it Project Files";
+        };
+
+        "<leader>s." = {
+          action = "oldfiles";
+          options.desc = "[S]earch Recent Files";
+        };
+
+        "<leader>s/" = {
+          action = "live_grep";
+          options.desc = "Grep (root dir)";
+        };
+      };
+
+      settings.defaults = {
+        file_ignore_patterns = [
+          "^.git/"
+        ];
+        set_env.COLORTERM = "truecolor";
+      };
+    };
+  };
+}
